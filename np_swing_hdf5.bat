3612ed22493fdbfdfae93ca206badc0d82bff8da
Florent Langlois <langlois@synchrotron-soleil.fr>
2021-04-27 11:00:53 +0200

    Add files via upload

---
+++ np_swing_hdf5.bat
@@ -0,0 +1,4 @@
+D:\Foxtrot-3.5.2-3645-2019-02-11-164214\python\Python27\python.exe np_swing_hdf5_jp.py^
+ -f1 Z:\com-swing\2020\Run5\nanoprobe3d\S02000-02999\nanoprobe3D_^
+ -f2 Z:\com-swing\published-data\2020\Run5\NanoprobeSWING_Nov2020\Interfero_files\NPSWING_2020_^
+ -num1 2411 -num2 2537 > np_swing_hdf5.log
