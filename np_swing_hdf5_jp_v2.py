b704cb782692d6fbab46143fc3eed52e4c4e48d8
Florent Langlois <langlois@synchrotron-soleil.fr>
2021-04-28 13:15:49 +0200

    version with nxs file writing OK

--- np_swing_hdf5_jp_v2.py
+++ np_swing_hdf5_jp_v2.py
@@ -1,5 +1,4 @@
-# Author: FL
-# modifs : JP
+# Author: FL 26/04/2021
 from argparse import ArgumentParser
 import h5py
 import csv
@@ -8,11 +7,6 @@ import os
 import random
 from os import walk
 
-class BuildError(Exception):
-    pass
-  
-def list_keys_h5(h5_file):
-    return [key for key in h5_file.keys()]
 
 #Gate_index;Calc-Gated_Sample-X;Calc-Gated_Sample-Z;Calc-Gated_Sample-Rz;Calc-Gated_Sample-Rx;Calc-Gated_Sample-Rs;Calc-Gated_FZPCS-X;Calc-Gated_FZPCS-Z;Calc-Gated_FZPCS-Rx;Calc-Gated_FZPCS-Rz;Raw-Gated_Sample-Xe;Raw-Gated_Sample-Xi;Raw-Gated_Sample-Ze;Raw-Gated_Sample-Zi;Raw-Gated_Sample-Zo;Raw-Gated_FZPCS-Xe;Raw-Gated_FZPCS-Xi;Raw-Gated_FZPCS-Ze;Raw-Gated_FZPCS-Zi;
 #------------------------------------------------------------------------------
@@ -29,61 +23,67 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     
-    if args.h5_input_file_root:
-        the_h5_file = args.h5_input_file_root
+    if args.csv_input_directory:
+        csv_input_directory = args.csv_input_directory
     else:
-        raise BuildError
-    if args.csv_input_file_root:
-        the_csv_file = args.csv_input_file_root
+        raise BaseException("No CSV input directory specified")
+
+    if args.nxs_output_directory:
+        nxs_output_directory = args.nxs_output_directory
     else:
-        raise BuildError
+        raise BaseException("No NXS output directory specified")
+    
     if args.num1:
         num1 = int(args.num1)
     if args.num2:
         num2 = int(args.num2)
     
-    # get directory
-    h5_dir = os.path.dirname(args.h5_input_file_root)
-    csv_dir = os.path.dirname(args.csv_input_file_root)
-    (_,_,h5_filenames) = walk(h5_dir).next()
+    # get filenames 
+    csv_dir = os.path.dirname(csv_input_directory)
     (_,_,csv_filenames) = walk(csv_dir).next()
-    
-    print "h5_dir = ", h5_dir
-    print "csv_dir = ", csv_dir
-    
+
+    print "csv_input_directory = ", csv_dir
+    print "nxs_output_directory = ", nxs_output_directory
+
+    columns_dict = { "0":"Gate_index",
+                    "1":"Calc-Gated_Sample-X"}
+
     while num1 <= num2:
-        for f in h5_filenames:
-            if f.find(str(num1)) != -1:
-                the_h5_file = f
-                print "--- nxs_file = ", the_h5_file
-                break
+        the_csv_file = ""
+        the_nxs_file = ""
         for f in csv_filenames:
             if int(f[f.rfind('-')+1:f.rfind('.')]) == int(num1):
                 the_csv_file = f
+                the_nxs_file = os.path.splitext(f)[0]+'.nxs'
                 break
-        print "h5_file = ", the_h5_file
+            else:
+                exit()
+            
         print "csv_file = ", the_csv_file
+        print "nxs_file = ", the_nxs_file
+
         print "num1 = ", num1
         
         num1+=1
         
     
         #==========================================================================   
-        #read csv file and 
-        calc_gated_sample_tx = []
-        calc_gated_sample_tz = []
-        calc_gated_fzp_tx    = []
-        calc_gated_fzp_tz    = []
-        substraction_tx      = []
-        substraction_tz      = []
-        substraction_noisy100tx      = []
-        substraction_noisy100tz   = []
-        substraction_noisy50tx      = []
-        substraction_noisy50tz   = []
-        substraction_noisy25tx     = []
-        substraction_noisy25tz   = []
-        substraction_noisy10tx      = []
-        substraction_noisy10tz   = []
+        # read csv file and 
+        calc_gated_sample_tx    = []
+        calc_gated_sample_tz    = []
+        calc_gated_fzp_tx       = []
+        calc_gated_fzp_tz       = []
+        substraction_tx         = []
+        substraction_tz         = []
+        substraction_noisy100tx = []
+        substraction_noisy100tz = []
+        substraction_noisy50tx  = []
+        substraction_noisy50tz  = []
+        substraction_noisy25tx  = []
+        substraction_noisy25tz  = []
+        substraction_noisy10tx  = []
+        substraction_noisy10tz  = []
+
         with open(csv_dir+'/'+the_csv_file) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=';')
             line_count = 0
@@ -102,8 +102,6 @@ if __name__ == "__main__":
                         calc_gated_sample_tz.append(float(a_row[2]))
                         calc_gated_fzp_tx.append(float(a_row[6]))
                         calc_gated_fzp_tz.append(float(a_row[7]))
-#                        substraction_tx.append(float(a_row[1])) 
-#                        substraction_tz.append(float(a_row[2])) 
                         substraction_tx.append(float(a_row[1]) - float(a_row[6])) 
 #                       substraction_tz.append(float(a_row[2]) - float(a_row[7]))
 #   Probleme avec Ze et Zo. On utilise gated_Zi pour le sample 
@@ -124,15 +122,13 @@ if __name__ == "__main__":
                         pass
                         #print ("maybe end of the file...", )
             
-            print "calc_gated_sample_tx: ",calc_gated_sample_tx
+            print "\ncalc_gated_sample_tx: ",calc_gated_sample_tx
             print('nb lines',line_count)
             
         #==========================================================================   
-        # read nxs file
-        images = None
-        f = h5py.File(h5_dir+'/'+the_h5_file, 'a')
-        
-        key_top = list_keys_h5(f)
+        # create nxs file
+        f = h5py.File(nxs_output_directory +'/'+ "flo.nxs", 'a')
+        f.create_group("/entry/scan_data")
         
         #==========================================================================       
         
@@ -140,8 +136,8 @@ if __name__ == "__main__":
  #       calc_gated_sample_tz_nx = f['/'+key_top[0]+'/scan_data'].create_dataset(u"historised_sample_tz", data=calc_gated_sample_tz)
  #       calc_gated_fzp_tx_nx    = f['/'+key_top[0]+'/scan_data'].create_dataset(u"historised_fzp_tx", data=calc_gated_fzp_tx)
  #       calc_gated_fzp_tz_nx    = f['/'+key_top[0]+'/scan_data'].create_dataset(u"historised_fzp_tz", data=calc_gated_fzp_tz)
-        substraction_tx_nx      = f['/'+key_top[0]+'/scan_data'].create_dataset(u"historised_relative_sample_tx", data=substraction_tx)
-        substraction_tz_nx      = f['/'+key_top[0]+'/scan_data'].create_dataset(u"historised_relative_sample_tz", data=substraction_tz)
+        substraction_tx_nx      = f["/entry/scan_data"].create_dataset(u"historised_relative_sample_tx", data=substraction_tx)
+    #substraction_tz_nx      = f['/entry/scan_data'].create_dataset(u"historised_relative_sample_tz", data=substraction_tz)
  #       substraction_noisytx_nx      = f['/'+key_top[0]+'/scan_data'].create_dataset(u"historised_relative_sample_noisy100tx", data=substraction_noisy100tx)
  #       substraction_noisytz_nx      = f['/'+key_top[0]+'/scan_data'].create_dataset(u"historised_relative_sample_noisy100tz", data=substraction_noisy100tz)
  #       substraction_noisytx_nx      = f['/'+key_top[0]+'/scan_data'].create_dataset(u"historised_relative_sample_noisy50tx", data=substraction_noisy50tx)
@@ -151,6 +147,6 @@ if __name__ == "__main__":
  #       substraction_noisytx_nx      = f['/'+key_top[0]+'/scan_data'].create_dataset(u"historised_relative_sample_noisy10tx", data=substraction_noisy10tx)
  #       substraction_noisytz_nx      = f['/'+key_top[0]+'/scan_data'].create_dataset(u"historised_relative_sample_noisy10tz", data=substraction_noisy10tz)
         f.close()
-        print key_top[0], "done"
+        print "done"
 
  
